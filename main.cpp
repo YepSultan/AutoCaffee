5797bd39985e0951e1f6e1d12547291026ebf01b
YepSultan <evening.tea@mail.ru>
2025-07-21 12:13:34 +0500

    New features: switch-case and cleaned-up code

--- main.cpp
+++ main.cpp
@@ -20,29 +20,80 @@ public:
 class Booking {
 private:
 
-	string fullnames, id, times, bar, menu; 
-	int age;
+	string fullnames, id, drink, bar, menu; 
+	int orderCode;
 
 
 public:
 	void getOrder(){
+	
+
+	switch(orderCode) 
+	{
+		case 1: drink = "Iced Latte"; break; 
+		case 2: drink = "Thai Coffee"; break;
+		case 3: drink = "Limonade"; break;
+		case 4: drink = "Expresso"; break;
+		case 5: drink = "Milk-Chocko"; break;
+
+	}
+
+
+
+
+
 		out << "Enter your fullnam: ";
 		getline(cin, fullnames);
 		out << "What's your order?: ";
-		in >> age;
+		in >> orderCode;
 		in.ignore();
-		out << fullnames << " | " << "Order: "<< age << nl;
+		out << fullnames << " | " << "Order: "<< orderCode << nl;
 	}
 };
  
 int main(int argc, char const *argv[])
 {
 
+	string input, ordering;
+	ordering = "Yes";
 	Booking b1;
-	Booking b2;
-	Booking b3;
-	b1.getOrder();
-	b2.getOrder();
-	b3.getOrder();
+	
+	out << nl;
+	out << "---  Menu ---" << nl;
+	out << "1. Iced Latte " << nl;
+	out << "2. Thai Coffee" << nl;
+	out << "3. Limonade " << nl;
+	out << "4. Expresso " << nl;
+	out << "5. Milk-Chocko " << nl;
+
+
+	out << "Wanna order something? [Yes/No]: ";
+	in >> input;
+	in.ignore();
+
+	if (input == ordering)
+	{
+		b1.getOrder();
+	}
+	else if (input == "No")
+	{
+		out << nl;
+		out << "Thanks for coming and good luck!" << nl;
+		out << nl;
+	}
+	else
+	{
+		out << nl;
+		out << "<<<<<<<----------------->>>>>>>" << nl;
+		out << "It's seems like u tryna do something else :/ " << nl;
+		out << "<<<<<<<----------------->>>>>>>" << nl;
+		out << nl;
+	}
+
+
+
+
+
+
 	return 0;
 }
